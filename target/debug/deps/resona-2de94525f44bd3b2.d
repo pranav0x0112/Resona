180dc9f45e43c6f7381c5b9f17cc9863da391848
prxnav2005 <pranavmofficial2005@gmail.com>
2025-06-27 12:53:24 +0530

    what happens when u dont add gitignore

---
+++ target/debug/deps/resona-2de94525f44bd3b2.d
@@ -0,0 +1,9 @@
+/home/prawns/Resona/target/debug/deps/resona-2de94525f44bd3b2: src/main.rs src/cpu.rs src/mem.rs src/isa.rs src/sim.rs
+
+/home/prawns/Resona/target/debug/deps/resona-2de94525f44bd3b2.d: src/main.rs src/cpu.rs src/mem.rs src/isa.rs src/sim.rs
+
+src/main.rs:
+src/cpu.rs:
+src/mem.rs:
+src/isa.rs:
+src/sim.rs:
---
+++ target/debug/resona.d
@@ -0,0 +1,1 @@
+/home/prawns/Resona/target/debug/resona: /home/prawns/Resona/src/cpu.rs /home/prawns/Resona/src/isa.rs /home/prawns/Resona/src/main.rs /home/prawns/Resona/src/mem.rs /home/prawns/Resona/src/sim.rs
