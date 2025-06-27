79d0d032bd24269bf9f69511debd970e4312194e
prxnav2005 <pranavmofficial2005@gmail.com>
2025-06-27 12:21:47 +0530

    feat: implement CPU state and step function for RV32I execution

---
+++ cpu.rs
@@ -0,0 +1,37 @@
+use crate::isa::Instruction;
+
+pub struct CPU {
+    pub regs: [u32; 32],
+    pub pc: u32,
+}
+
+impl CPU {
+    pub fn new() -> Self {
+        Cpu {
+            regs: [0; 32],
+            pc: 0,
+        }
+    }
+
+    pub fn step(&mut self, instr: Instruction) {
+        match instr {
+            Instruction::Add { rd, rs1, rs2 } => {
+                let result = self.regs[rs1] + self.regs[rs2]; 
+                self.write(rd, result);
+            }
+            Instruction::Addi { rd, rs1, imm } => {
+                let result = self.regs[rs1].wrapping_add(imm as u32);
+                self.write(rd, result);
+            }
+            Instruction::Unknown(val) => {
+                println!("Unknown instruction: 0x{:08x} @ pc=0x{:08x}", val, self.pc);
+            }
+        }
+    }
+
+    fn write(&mut self, reg: usize, value: u32) {
+        if reg != 0 {
+            self.regs[reg] = val;
+        }
+    }
+}
