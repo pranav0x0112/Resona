859223cb91e8d188503a940b610c517b55188776
prxnav2005 <pranavmofficial2005@gmail.com>
2025-06-27 12:55:37 +0530

    add .gitignore

--- target/debug/deps/resona-2de94525f44bd3b2.d
+++
@@ -1,9 +0,0 @@
-/home/prawns/Resona/target/debug/deps/resona-2de94525f44bd3b2: src/main.rs src/cpu.rs src/mem.rs src/isa.rs src/sim.rs
-
-/home/prawns/Resona/target/debug/deps/resona-2de94525f44bd3b2.d: src/main.rs src/cpu.rs src/mem.rs src/isa.rs src/sim.rs
-
-src/main.rs:
-src/cpu.rs:
-src/mem.rs:
-src/isa.rs:
-src/sim.rs:
--- target/debug/resona.d
+++
@@ -1,1 +0,0 @@
-/home/prawns/Resona/target/debug/resona: /home/prawns/Resona/src/cpu.rs /home/prawns/Resona/src/isa.rs /home/prawns/Resona/src/main.rs /home/prawns/Resona/src/mem.rs /home/prawns/Resona/src/sim.rs
